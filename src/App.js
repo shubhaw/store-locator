c6eeed28b285cc56be23c32180d11fb8f79299b9
Shubhaw Kumar <dev.shubhaw@gmail.com>
2019-10-02 21:33:33 +0530

    added firebase to project and private route component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { AuthProvider } from './hoc/Auth/Auth';
 import './App.css';
 import Layout from './containers/Layout/Layout';
 import AddStore from './containers/AddStore/AddStore';
 import Stores from './containers/Stores/Stores';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Login from './containers/Login/Login';
 
 class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Route path="/download-all" component={Stores} />
-            <Route path="/" exact component={AddStore} />
-            <Redirect to="/" />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    )
-  }
+    render() {
+        return (
+            <AuthProvider>
+                <BrowserRouter>
+                    <Layout>
+                        <Switch>
+                            <PrivateRoute path="/download-all" component={Stores} />
+                            <Route path="/" exact component={AddStore} />
+                            <Route path="/login" exact component={Login} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </Layout>
+                </BrowserRouter>
+            </AuthProvider>
+        )
+    }
 }
 
 export default App;
